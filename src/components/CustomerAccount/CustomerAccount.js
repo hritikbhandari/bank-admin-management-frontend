645eca6a6e04e376e8f4324002c59c32fb1c055d
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-11-09 11:08:29 +0530

    Update CustomerAccount.js

--- src/components/CustomerAccount/CustomerAccount.js
+++ src/components/CustomerAccount/CustomerAccount.js
@@ -1,7 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import "./style.css"
 //eslint-disable-next-line 
 export default function (props) {
+
+    const [accountNumber,setAccountNumber]=useState('');
+    const [customerNumber,setCustomerNumber]=useState('');
+    const [branchId,setBranchId]=useState('');
+    const [openingBalance,setOpeningBalance]=useState([]);
+    const [openingDate,setOpeningDate]=useState('');
+    const [accountType,setAccountType]=useState([]);
+    const [accountStatus,setAccountStatus]=useState([]);
+
+    
+        const BranchHandler=(selectedOption)=>{
+            setBranchId(selectedOption);
+            console.log("Branch Handle",selectedOption);
+        }
+        
+        const StatusHandler=(selectedOption)=>{
+            setAccountStatus(selectedOption);
+            console.log("Acccount Status",selectedOption);
+        }
+        
+        const accountTypeHandler=(selectedOption)=>{
+            setAccountType(selectedOption);
+            console.log("Branch Handle",selectedOption);
+        }
+        const Add={}
+                             
+   
+    
     return (
 
         <div class="login-form">
@@ -13,14 +41,16 @@ export default function (props) {
                     <div class="form-group ">
                         <label class="form-label">Account Number</label>
                         <input class="form-control" type="text"
-
+                            onChange={(e)=>setAccountNumber(e.target.value)}
+                            value={accountNumber}
                             placeholder="Account Number"
                             required />
                     </div>
                     <div class="form-group ">
                         <label class="form-label">Customer Number</label>
                         <input class="form-control" type="text"
-
+                            onChange={(e)=>setCustomerNumber(e.target.value)}
+                            value={customerNumber}
                             placeholder="Customer Number"
                             required />
                     </div>
@@ -28,42 +58,43 @@ export default function (props) {
                         <label class="form-label">Branch Id</label>
                         <select class="form-select form-select-sm" aria-label=".form-select-sm example">
                             <option selected>Choose</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
+                            <option value={branchId} onChange={BranchHandler}></option>
+                         
                         </select>
                     </div>
                     <div class="form-group ">
                         <label class="form-label">Opening Balance</label>
                         <input class="form-control" type="number" min="5000"
-
+                              onChange={(e)=>setOpeningBalance(e.target.value)}
+                              value={openingBalance}
                             placeholder="atleast 5000"
                             required />
                     </div>
                     <div class="form-group ">
                         <label class="form-label">Opening Date</label>
                         <input class="form-control" type="date"
-
-                            // placeholder="Customer Number"
+                         
+                         onChange={(e)=>setOpeningDate(e.target.value)}
+                         value={openingDate}
                             required />
                     </div>
                     <div class="form-group ">
                         <label class="form-label">Account Type</label>
                         <select class="form-select form-select-sm" aria-label=".form-select-sm example">
                             <option selected>Choose</option>
-                            <option value="1">Current</option>
-                            <option value="2">Savings</option>
+                            <option value={accountType} onChange={accountTypeHandler}></option>
+                           
                         </select>
                     </div>
                     <div class="form-group ">
                         <label class="form-label">Account Status</label>
                         <select class="form-select form-select-sm" aria-label=".form-select-sm example">
                             <option selected>Choose</option>
-                            <option value="1">Active</option>
-                            <option value="2">Inactive</option>
+                            <option value={accountStatus} onChange={StatusHandler}>Active</option>
+                        
                         </select>
                     </div>
-                    <input class="btn btn-success w-100" type="submit" value="Add" />
+                    <input class="btn btn-success w-100" type="submit" onClick={Add} value="Add"  />
                 </form>
             </div>
         </div>
